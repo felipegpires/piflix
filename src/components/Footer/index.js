cfb84bea1e760ed3f01c3748d3eb963dc7769448
felipegpires <felipegpires@gmail.com>
2020-07-27 17:59:51 -0300

    Atualização

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,13 +5,13 @@ import Rodape from '../../assets/img/rodape.png';
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.facebook.com/felipe.gomespires">
+      <a href="https://www.facebook.com/felipe.gomespires" target="_blank" rel="noopener noreferrer">
         <img src={Rodape} alt="Felipe Gomes Pires"/>
       </a>
       <p>
         Orgulhosamente criado por
         {' '}
-        <a href="https://www.facebook.com/felipe.gomespires">
+        <a href="https://www.facebook.com/felipe.gomespires" target="_blank" rel="noopener noreferrer">
           Felipe Gomes Pires
         </a>
       </p>
